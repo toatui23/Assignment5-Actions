cb43107bd8746a4cfde139c188cfa4b404d61ee4
Noah <seumalon@oregonstate.edu>
2020-02-27 17:10:04 -0800

    first deliverable

--- task.py
+++ task.py
@@ -1,2 +1,8 @@
+import math
+
+
 def firstrun():
     return "success"
+
+def CircleArea(radius):
+    return round(math.pi*(radius*radius),2)
--- tests.py
+++ tests.py
@@ -12,6 +12,10 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def circletest(self):
+        expected = task.CircleArea(1)
+        self.assertEqual(expected, 3.14)
+
 
 if __name__ == '__main__':
     unittest.main()
